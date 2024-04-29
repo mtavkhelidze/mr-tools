b777c71bfc1382060a3310af2d4c14e477865722
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2024-04-29 09:01:33 +0400

    Finish mrecho

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use clap::{Parser, ArgAction};
 
+#[derive(Debug)]
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
     /// Input text
-    #[arg(value_name = "INPUT")]
-    input: String,
+    #[arg(value_name = "text", required = true)]
+    strings: Vec<String>,
 
     /// Do not print newline
     #[arg(short, action(ArgAction::SetTrue))]
@@ -15,4 +16,6 @@ struct Cli {
 
 fn main() {
     let cli = Cli::parse();
+    let nl = if cli.no_newlines.unwrap_or(false) { "" } else { "\n" };
+    print!("{}{}", cli.strings.join(" "), nl);
 }
