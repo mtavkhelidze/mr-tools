f9f28d339d923170f46d696eece0bdd207c0c448
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2024-05-02 11:41:25 +0400

    CH-03 Restructure code

--- mrcat/src/lib.rs
+++ mrcat/src/lib.rs
@@ -1,45 +1,24 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+pub mod lib {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
 
-use clap::{ArgAction, Parser};
+    pub type MRResult<T> = Result<T, Box<dyn std::error::Error>>;
 
-type MRResult<T> = Result<T, Box<dyn std::error::Error>>;
-
-#[derive(Debug, Parser)]
-#[command(version, about, long_about = None)]
-pub struct Arguments {
-    /// Input files
-    #[arg(value_name = "file(s)", required = true)]
-    file_names: Vec<String>,
-
-    /// Output line numbers
-    #[arg(short, action(ArgAction::SetTrue))]
-    number_lines: Option<bool>,
-
-    /// Number non-blank lines
-    #[arg(short = 'b', action(ArgAction::SetTrue))]
-    number_non_blanks: Option<bool>,
-}
-
-pub fn get_args() -> MRResult<Arguments> {
-    let config = Arguments::parse();
-    Ok(config)
-}
-
-pub fn open(filename: &str) -> MRResult<Box<dyn std::io::BufRead>> {
-    match filename {
-        "-" => Ok(Box::new(std::io::stdin().lock())),
-        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    fn open(filename: &str) -> MRResult<Box<dyn std::io::BufRead>> {
+        match filename {
+            "-" => Ok(Box::new(std::io::stdin().lock())),
+            _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+        }
     }
-}
 
-pub fn cat(config: &Arguments) -> MRResult<()> {
-    let result = config.file_names.iter().map(|filename| {
-        open(filename).map(|buf_reader| {
-            buf_reader
-                .lines()
-                .for_each(|line| println!("{}", line.unwrap()));
-        })
-    });
-    result.collect()
+    pub fn cat(file_names: &Vec<String>) -> MRResult<()> {
+        let result = file_names.iter().map(|filename| {
+            open(filename).map(|buf_reader| {
+                buf_reader
+                    .lines()
+                    .for_each(|line| println!("{}", line.unwrap()));
+            })
+        });
+        result.collect()
+    }
 }
--- mrcat/src/main.rs
+++ mrcat/src/main.rs
@@ -1,7 +1,29 @@
-use mrcat;
-use mrcat::{cat, get_args};
+use clap::{ArgAction, Parser};
+
+use mrcat::lib::{cat, MRResult};
 
 fn main() {
     let config = get_args().unwrap();
-    cat(&config).unwrap();
+    cat(&config.file_names).unwrap();
+}
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Arguments {
+    /// Input files
+    #[arg(value_name = "file(s)", required = true)]
+    file_names: Vec<String>,
+
+    /// Output line numbers
+    #[arg(short, action(ArgAction::SetTrue))]
+    number_lines: Option<bool>,
+
+    /// Number non-blank lines
+    #[arg(short = 'b', action(ArgAction::SetTrue))]
+    number_non_blanks: Option<bool>,
+}
+
+fn get_args() -> MRResult<Arguments> {
+    let config = Arguments::parse();
+    Ok(config)
 }
